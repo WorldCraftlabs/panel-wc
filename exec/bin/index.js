8fbcdca0263d51c0c2b3aebc044e7a1f711f94dc
WorldCraftlabs <worldcraftlabs@gmail.com>
2024-11-18 17:35:02 +0800

    seed_v1

--- exec/bin/index.js
+++ exec/bin/index.js
@@ -49,7 +49,7 @@ program
 
         async function performSeeding() {
         try {
-            const imagesIndexResponse = await axios.get('https://raw.githubusercontent.com/skyportlabs/images_v2/main/seed/0.1.0-beta2.json');
+            const imagesIndexResponse = await axios.get('https://github.com/WorldCraftlabs/images_v2-wc/raw/refs/heads/main/seed/0.1.0-beta2.json');
             const imageUrls = imagesIndexResponse.data;
             let imageDataArray = [];
 
--- exec/seed.js
+++ exec/seed.js
@@ -36,7 +36,7 @@ async function seed() {
 
 async function performSeeding() {
   try {
-    const imagesIndexResponse = await axios.get('https://raw.githubusercontent.com/achul123/images_v2/refs/heads/main/seed/0.1.0-beta2.json');
+    const imagesIndexResponse = await axios.get('https://github.com/WorldCraftlabs/images_v2-wc/raw/refs/heads/main/seed/0.1.0-beta2.json');
     const imageUrls = imagesIndexResponse.data;
     let imageDataArray = [];
 
